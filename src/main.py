352572636a9183c91618fa30bae1215808a7876a
Artyom Boyarov <artyom.boyarov@gmail.com>
2021-08-18 19:31:46 +0200

    added some tests

--- src/main.py
+++ src/main.py
@@ -116,7 +116,7 @@ def compare_password_hash(user_password, db_password):
 client.on_connect = on_connect
 client.on_message = on_message
 # client.connect("com-ra-api.co.uk")
-client.loop_start()
+# client.loop_start()
 print("MQTT client started")
 
 app = FastAPI()
