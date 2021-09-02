3d51c45c3468147474e5506a3dc5b63deb129005
root <root@vultr.guest>
2021-09-02 10:00:21 +0000

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 cd src
-nohup uvicorn main:app --host 0.0.0.0 --ssl-keyfile=/etc/letsencrypt/live/com-ra-api.co.uk/privkey.pem --ssl-certfile=/etc/letsencrypt/live/com-ra-api.co.uk/fullchain.pem  --port 443
+DEBUG_MODE=False nohup uvicorn main:app --host 0.0.0.0 --ssl-keyfile=/etc/letsencrypt/live/com-ra-api.co.uk/privkey.pem --ssl-certfile=/etc/letsencrypt/live/com-ra-api.co.uk/fullchain.pem  --port 443
