aa100b6573a7e869f5848f69f4c39e591da835ed
Artyom vultr <artyom.boyarov@gmail.com>
2021-09-17 16:49:22 +0000

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+VER="1.3.1"
 cd src
-DEBUG_MODE=False nohup uvicorn main:app --host 0.0.0.0 --ssl-keyfile=/etc/letsencrypt/live/com-ra-api.co.uk/privkey.pem --ssl-certfile=/etc/letsencrypt/live/com-ra-api.co.uk/fullchain.pem  --port 443
+DEBUG_MODE=False VER=$VER nohup uvicorn main:app --host 0.0.0.0 --ssl-keyfile=/etc/letsencrypt/live/com-ra-api.co.uk/privkey.pem --ssl-certfile=/etc/letsencrypt/live/com-ra-api.co.uk/fullchain.pem  --port 443
