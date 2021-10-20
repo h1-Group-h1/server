9d29f1ed0387d111c4641c3bbe16fddf082a5636
Artyom Boyarov <artyom.boyarov@gmail.com>
2021-10-20 15:48:16 +0100

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+echo "Deploy script - will deploy application and mosquitto broker"
+
 VER="1.3.1"
 cd src
-DEBUG_MODE=False VER=$VER nohup uvicorn main:app --host 0.0.0.0 --ssl-keyfile=/etc/letsencrypt/live/com-ra-api.co.uk/privkey.pem --ssl-certfile=/etc/letsencrypt/live/com-ra-api.co.uk/fullchain.pem  --port 443
+DEBUG_MODE=False VER=$VER nohup uvicorn main:app --host 0.0.0.0 --ssl-keyfile=/etc/letsencrypt/live/com-ra-api.co.uk/privkey.pem --ssl-certfile=/etc/letsencrypt/live/com-ra-api.co.uk/fullchain.pem  --port 443 & mosquitto -c /etc/mosquitto/mosquitto.conf
