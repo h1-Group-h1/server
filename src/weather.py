036365509c60a4b4bf611491b519038c875ed073
Stefan van Biljon <stefan@Stefans-Air.broadband>
2021-10-23 17:11:34 +0100

    changed weather api, added example in multiline string

--- src/weather.py
+++ src/weather.py
@@ -1,43 +1,59 @@
 import requests
-# requires a private api key
+
+# requires a private api key from weatherbit.io
 config_file = "api_key.txt"
 
 with open(config_file, 'r') as f:
-	api_key = f.read()
+	API_KEY = f.read()
 
       
+def get_weather(city, country, API_KEY):
+    BASE_URL =  f'https://api.weatherbit.io/v2.0/current?city={city}&country=GB&&key={API_KEY}'
+    
+    print(BASE_URL)
+    response = requests.get(BASE_URL)
 
 
-def get_weather(search_city, api_key):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={search_city}&appid={api_key}'
-    print(url)
-    print(api_key)
+    print(response.json())
 
-    s = requests.Session()
-    r = s.get(url)
-    print(r.headers)
-    print(r.text)
+    if response:
+        data = (response.json())['data'][0]
+        country = data['country_code']
+        percent_clouds = data['clouds']
+        sunset = data['sunset']
+        sunrise = data['sunrise']
+        current_temp = data['temp']
+        feels_like = data['app_temp']
+        lat = data['lat']
+        lon = data['lon']
+        
+        
+        result = {
+            'country': country, 
+            'cloud_cover': percent_clouds, 
+            'sunset': sunset, 
+            'sunrise': sunrise, 
+            'temperature': current_temp, 
+            'feelsLike': feels_like, 
+            'latitude': lat, 
+            'longitude': lon}
 
-    result = requests.get(url)
-
-    print(result)
-    print(result.json())
-
-    if result:
-        json = result.json()
-        city = json['name']
-        country = json['sys']
-        temp_kelvin = json['main']['temp']
-        temp_celsius = temp_kelvin-273.15
-        weather1 = json['weather'][0]['main']
-        final = [city, country, temp_kelvin, 
-                 temp_celsius, weather1]
-        return final
+        return result
     else:
         print("Something went wrong.")
+        
+def get_location():
+    base = dict(requests.get('https://ipinfo.io').json())
+    
+    city = base['city']
+    country = base['country']
+
+    return[city, country]
 
 
-my_city = dict(requests.get('https://ipinfo.io').json())['city']
-print(my_city)
-my_weather = get_weather(my_city.lower(), api_key)
-print(my_weather)
+# example of how to get weather for your location
+'''
+my_city, my_country = get_location()
+
+print(get_weather(my_city, my_country, API_KEY))
+'''
